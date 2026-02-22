86d5575f006a87221faf6d58b8c7416b49a669bf
Grzegorz Owsianka <grzegorz.owsianka84.91@gmail.com>
2026-02-22 13:07:59 +0100

    refactor(test): improve TransactionTest readability

--- src/test/java/com/owsiankagrzegorz/expensetracker/model/TransactionTest.java
+++ src/test/java/com/owsiankagrzegorz/expensetracker/model/TransactionTest.java
@@ -5,14 +5,16 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 
 public class TransactionTest {
+
+    LocalDate date = LocalDate.of(2025, 9, 7);
     @Test
-    void testTransactionCreation() {
+    void shouldCreateTransactionWithGivenValues() {
         Transaction t = new Transaction(1L, 200.0, "Transport", LocalDate.of(2025, 9, 7), "WYDATEK");
 
         assertEquals(1L, t.getId());
         assertEquals(200.0, t.getAmount());
         assertEquals("Transport", t.getCategory());
-        assertEquals(LocalDate.of(2025, 9, 7), t.getDate());
+        assertEquals(date, t.getDate());
         assertEquals("WYDATEK", t.getType());
     }
 }
